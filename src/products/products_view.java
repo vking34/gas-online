c453f5f2c2363281b5698cca02451ec16703a762
VKing34 <lzzvkingzzl@gmail.com>
2018-04-23 08:45:15 +0700

    add abouts, products, tips

---
+++ src/products/products_view.java
@@ -0,0 +1,19 @@
+package products;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "products_view")
+public class products_view extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/productSites/products.html").forward(request,response);
+    }
+}
